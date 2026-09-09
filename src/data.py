e53665b13e32d27c9a5d3650ea5eeb7cf843240e
Ernie-Johnson <erniehjohnson@gmail.com>
2026-09-10 00:00:44 +0100

    added function to complete an epoch
    Set up factored matrices and created function to complete an epoch

--- src/data.py
+++ src/data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import math
 
 df = pd.read_csv("../data/ml-100k/u.data", sep="\t", header=None)
@@ -20,3 +21,30 @@ for user_id, user_data in grouped:
 
 train_df = pd.concat(train_parts)
 test_df = pd.concat(test_parts)
+
+train_pivot = train_df.pivot_table(index="user_id", columns="item_id", values="rating")
+dim_rows, dim_columns = train_pivot.shape
+k = 10
+U_matrix = np.random.rand(dim_rows, k) # each row represents one user (943 rows)
+V_matrix = np.random.rand(dim_columns, k) # each row represents a movie (1613 rows)
+
+movie_to_index = {} # creating a mapping between movie_id and V row index
+
+for index, movie_id in enumerate(train_pivot.columns):
+    movie_to_index[movie_id] = index
+
+
+learning_rate = 0.01
+def epoch(U_matrix, V_matrix, train_df, learning_rate):
+    for row in train_df.itertuples():
+        i, j = (row.user_id, row.item_id)
+
+        u_i = U_matrix[i-1, :]
+        v_j = V_matrix[movie_to_index[j], :]
+
+        r_ij = row.rating
+        error_ij = r_ij - np.dot(u_i, v_j)
+
+        U_matrix[i-1, :] = u_i + learning_rate * error_ij * v_j
+        V_matrix[movie_to_index[j], :] = v_j + learning_rate * error_ij * u_i
+    return (U_matrix, V_matrix)
